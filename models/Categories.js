41a3f8da6c248e70bb7c463a9d675b48981b7dfa
areedy <a.reedy@gmail.com>
2023-01-11 20:01:43 -0500

    ading Categories model

---
+++ models/Categories.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Categories extends Model {}
+
+Favorites.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'categories',
+    }
+);
+
+module.exports = Categories;
