0c3f325a60082727ab79893e8b6d62f2942581cf
Andrew <andrewg87@gmail.com>
2023-01-12 20:35:40 -0500

    updated index.js in api folder to include the new routes created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const recipeRoutes = require('./recipe-routes')
+const recipeRoutes = require('./recipe-routes');
+const favoriteRoutes = require('./favorite-routes');
+const reviewRoutes = require('./review-routes');
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
+router.use('/favorites', favoriteRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
