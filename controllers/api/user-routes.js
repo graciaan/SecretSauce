fda217466615008ab526966916894257c3a54e7c
Andrew <andrewg87@gmail.com>
2023-01-19 19:32:47 -0500

    added express-validator tech to validate new user creation

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const { body, validationResult } = require('express-validator');
 
 //Creates New User
 router.post('/', async (req, res) => {
@@ -28,26 +29,21 @@ router.post('/login', async (req, res) => {
         username: req.body.username,
       },
     });
-
     if (!dbUserData) {
       res
         .status(400)
         .json({ message: 'Incorrect username or password. Please try again!' });
       return;
     }
-
     const validPassword = await dbUserData.checkPassword(req.body.password);
-
     if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect username or password. Please try again!' });
       return;
     }
-
     req.session.save(() => {
       req.session.loggedIn = true;
-
       res
         .status(200)
         .json({ user: dbUserData, message: 'You are now logged in!' });
@@ -57,6 +53,26 @@ router.post('/login', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.post(
+  '/user',
+  // username must alphanumeric
+  body('username').isAlphanumeric(),
+  // password must be at least 6 chars long
+  body('password').isLength({ min: 6 }),
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      username: req.body.username,
+      password: req.body.password,
+    }).then(user => res.json(user));
+  },
+);
 
 //Logout
 router.post('/logout', (req, res) => {
