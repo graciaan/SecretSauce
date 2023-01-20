65008b7c1a7ea251ca4af1bd1ecf45efc966e523
areedy <a.reedy@gmail.com>
2023-01-19 19:37:10 -0500

    fixed typo in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,7 +18,7 @@ insert into recipes (title, description, ingredients, instructions, user_id, cat
 values ("Shrimp Scampi", "Marinated lemon and herb shrimp in a scampi sauce.", "shrimp, lemon, herb, scampi", "add the stuff and stir", 1, 4, "05-shrimpscampi.png"),
 ("Lasagna", "Homemade lasagna.", "noodles, sauce, cheese", "add the stuff and bake", 2, 2, "06-lasagna.png"),
 ("Chicken Fajitas", "Flavorful grilled chicken fajitas with cheddar cheese, guacamole, sour cream, and vegetables.", "chicken, cheese, guac, tortillas", "cook it then eat it", 3, 4, "07-chickenfajitas.png"),
-("Jambalaya", "Bring the kitchen sink and spice it up in this jambalaya dish.", "beans, rice, other stuff", "cook it", 23, 4, "08-jambalaya.png'"),
+("Jambalaya", "Bring the kitchen sink and spice it up in this jambalaya dish.", "beans, rice, other stuff", "cook it", 3, 4, "08-jambalaya.png'"),
 ("Egg Roll Noodle Bowl", "Asian dish incorporating egg roll ingredients with flavorful noodles.", "egg roll, noodle, bowl", "heat up and enjoy", 4, 1, "09-eggrollnoodle.png"),
 ("Chocolate Cake", "Freshly baked chocolate cake.", "cake mix, egg, water", "mix it all together and bake for a while", 1, 1, "10-chocolatecake.png");
 
