aa8938e906494bbc5b52fcb57388f1f5d61412cb
Andrew <andrewg87@gmail.com>
2023-01-19 22:14:46 -0500

    added new code to users model

--- models/Users.js
+++ models/Users.js
@@ -2,7 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-class Users extends Model {}
+// create our User model
+class Users extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 Users.init(
   {
